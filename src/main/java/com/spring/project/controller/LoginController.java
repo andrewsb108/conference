b924095e462218a2154165184e69645d980cdb98
andrewsb108 <andrey.barsuk1@gmail.com>
2021-08-09 10:48:34 +0300

    Fexed LoginController

--- src/main/java/com/spring/project/controller/LoginController.java
+++ src/main/java/com/spring/project/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.spring.project.dto.LoginDto;
 import com.spring.project.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
