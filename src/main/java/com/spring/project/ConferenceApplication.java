9ccee3827632deebae1a1106be278da1ca63a799
andrewsb108 <andrey.barsuk1@gmail.com>
2021-08-19 21:40:48 +0300

    Commit before added speaker role

--- src/main/java/com/spring/project/ConferenceApplication.java
+++ src/main/java/com/spring/project/ConferenceApplication.java
@@ -1,5 +1,6 @@
 package com.spring.project;
 
+import com.github.javafaker.Faker;
 import com.spring.project.model.Event;
 import com.spring.project.model.Topic;
 import com.spring.project.model.User;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Set;
 
@@ -47,6 +49,10 @@ class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Faker faker = new Faker(new Locale("uk"));
+
+
         User admin = new User();
         admin.setFirstName("admin");
         admin.setLastName("admin");
@@ -63,24 +69,37 @@ class MyRunner implements CommandLineRunner {
         speaker.setRoles(Set.of(Role.SPEAKER));
         repository.save(speaker);
 
+        for (int i = 0; i < 10; i++) {
+            User sp1 = new User();
+            sp1.setFirstName(faker.name().firstName());
+            sp1.setLastName(faker.name().lastName());
+            sp1.setEmail(faker.name().username() + "@gmail.com");
+            sp1.setPassword(encoder.encode("456"));
+            sp1.setRoles(Set.of(Role.SPEAKER));
+            repository.save(sp1);
+        }
+
+
         Random rnd = new Random();
-        Event event = new Event(0, "event 1", LocalDate.now().plusDays(rnd.nextInt(100)), LocalTime.now().plusMinutes(rnd.nextInt(100)), new ArrayList<>(), new ArrayList<>());
-        Event event1 = new Event(0, "event 2", LocalDate.now().plusDays(rnd.nextInt(100)), LocalTime.now().plusMinutes(rnd.nextInt(100)), new ArrayList<>(), new ArrayList<>());
-        Event event2 = new Event(0, "event 3", LocalDate.now().plusDays(rnd.nextInt(100)), LocalTime.now().plusMinutes(rnd.nextInt(100)), new ArrayList<>(), new ArrayList<>());
 
-        eventRepository.save(event);
-        eventRepository.save(event1);
-        eventRepository.save(event2);
+        for (int i = 0; i < 20; i++) {
+            Event event = new Event(0, faker.gameOfThrones().city(), LocalDate.now().plusDays(rnd.nextInt(100)), LocalTime.now().plusMinutes(rnd.nextInt(100)), new ArrayList<>(), new ArrayList<>());
+            eventRepository.save(event);
 
-        Topic firstTopic = new Topic(0, "some title");
-        Topic secondTopic = new Topic(0, "some next");
 
-        topicRepositiry.save(firstTopic);
-        topicRepositiry.save(secondTopic);
+            Topic firstTopic = new Topic(0, faker.gameOfThrones().character());
+            Topic secondTopic = new Topic(0, faker.gameOfThrones().character());
 
-        event.getTopicList().add(firstTopic);
-        event.getTopicList().add(secondTopic);
+            topicRepositiry.save(firstTopic);
+            topicRepositiry.save(secondTopic);
 
-        eventRepository.save(event);
+            event.getTopicList().add(firstTopic);
+            event.getTopicList().add(secondTopic);
+
+            eventRepository.save(event);
+        }
+
+
+
     }
 }
