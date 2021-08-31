01fd97a8f89e67bfba4e2abb9f378dd2654bccd3
andrewsb108 <andrey.barsuk1@gmail.com>
2021-08-31 18:32:04 +0300

    created UserServiceImplTest

---
+++ src/test/java/com/spring/project/service/impl/UserServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.spring.project.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void updateProfile() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+}
